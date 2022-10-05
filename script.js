3f2c56deb53a9f7d0aadf5121b871608f3c03212
Patrick Rutazuyaza <rutazuyazapatrick@gmail.com>
2022-10-04 21:32:33 -0500

    JSBasicsExertcise

---
+++ script.js
@@ -0,0 +1,89 @@
+// 1
+console.log(3);
+
+// 2
+/*
+Number
+String
+Boolean
+Null
+Undefined
+*/
+
+// 3
+/*
+Addition is +
+Subtraction is -
+Multiplication is *
+Division is /
+Modulus is %
+Exponentiation **
+
+EXTRAS
+increment ++
+decrement --
+*/
+
+// 4
+console.log(5 % 2); //1
+console.log(5 % 4); //1
+
+// EXTRA
+console.log(4 % 5); // 4
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Patrick".length + "Rutazuyaz".length); // 14
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// 1
+console.log("\/");
+
+// 2
+console.log(5 ** 4 - 9);
+// 5*5*5*5 = 625
+// 625 - 9 = 616
+
+// 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+// 1 - 2 + 4 - 3 = 0
+
+console.log(("I".length + "Am".length + "Iron".length + "Man".length) % 10);
+// 1 + 2 + 4 + 3 = 10
+// 10 % 10 = 0
+
+console.log(("I".length + "Am".length + "Iron".length + "Man".length) % 1);
+
+console.log(("I".length + "Am".length + "Iron".length + "Man".length) * 0);
+// 1 + 2 + 4 + 3 = 10
+// 10 * 0 = 0
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
+
+// WITH LET
+// let m = 25;
+// let c = 4;
+// let u = m + c;
+// console.log(u);
